035c6b467003dd49d59ae5caed0e07c8811ca1ce
Nathan Hartland <n.p.hartland@vu.nl>
2018-07-27 23:58:14 +0200

    l2d build files

--- conf.lua
+++ conf.lua
@@ -1,10 +1,23 @@
 if love.filesystem then
-	require 'rocks' ()
+    require 'rocks' ()
 end
 
 function love.conf(t)
-	t.identity = "golflike"
-	t.version = "11.1"
+    t.modules.joystick = false
+    t.modules.physics = false
+    t.title = 'golflike'
+    t.package = 'golflike'
+    t.loveVersion = '11.1'
+    t.version = "0.5b"
+    t.author = "Nathan Hartland"
+    t.email = "nphartland@gmail.com"
+    t.description = "A golf roguelike"
+    t.homepage = "https://github.com/nhartland/golflike"
+    t.identifier = nil
+    t.excludeFileList = {''}
+    t.compile = false
+    t.identity = "golflike"
+    t.version = "11.1"
     t.dependencies = {
         "forma ~> 0.2",
         "date ~> 2.1.2",
